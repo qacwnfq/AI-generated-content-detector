de0e49d6c3fb2ff2617b1f3fcede8d76825afef3
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2023-10-03 19:45:35 +0200

    add deploy command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  ssr: false,
-  devtools: { enabled: true }
-})
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   ssr: false,
+//   devtools: { enabled: true },
+//   routes: {
+//     '/': {prerender: true},
+//     '/*': {CORS: true}
+//   },
+//   allowFromAnyOrigin: false
+// })
+
+export default {
+  target: 'static',
+  router: {
+    base: ' http://qacwnfq.github.io/AI-generated-content-detector>'
+  }}
